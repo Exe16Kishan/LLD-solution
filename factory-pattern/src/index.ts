f29bdbd99803c6e8c7e02546a34bdc0117cf24d5
Exe16Kishan <kishanshukla53@gmail.com>
2026-09-09 17:04:05 +0530

    simple factory method

---
+++ factory-pattern/src/index.ts
@@ -0,0 +1,46 @@
+// there are two types of factory pattern are
+// simple factory  and factory pattern
+
+interface Send {
+  send(m: string): void;
+}
+
+class Sms implements Send {
+  send(m: string) {
+    console.log(`${m} message through sms`);
+  }
+}
+
+class Email implements Send {
+  send(m: string): void {
+    console.log(`${m} message through mail`);
+  }
+}
+
+class Factory {
+  static notification(type: string) {
+    switch (type) {
+      case "Sms":
+        return new Sms();
+
+      case "Email":
+        return new Email();
+
+      default:
+        console.log("unknown notification type")  
+      break;
+    }
+  }
+}
+
+
+class ClientNotification {
+    static create(type : string , m:string){
+        const notification = Factory.notification(type)
+        notification?.send(m)
+    }
+}
+
+
+const sms = ClientNotification.create("Sms","helllo")
+const mail = ClientNotification.create("Email","helllo")
